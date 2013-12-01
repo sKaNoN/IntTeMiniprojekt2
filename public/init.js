eae66965ff5685467a81588317cd12215126b095
MrHaribo <jonas_biedermann@hotmail.com>
2013-12-01 15:54:40 +0100

    Read JSON Obj vom Server, TODO: extract to dataservice.js und in "privatem" objekt zur verfügung stellen Han am Wucheend Java Script glernt und mittlerwil au scho chli jQuery

--- public/init.js
+++ public/init.js
@@ -5,6 +5,8 @@
     //sessionStorage.dataService = $.fn.DataService();  //stores data for one session
     //localStorage.dataService = $.fn.DataService();  //stores data with no expiration date
     
+    alert("test");
+    
     $.ajaxSetup({
         cache: false
     });
