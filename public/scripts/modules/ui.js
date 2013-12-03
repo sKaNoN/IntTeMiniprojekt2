66dba69fc1368cee164720cfe46d2aae06166d67
skanon <nosferatu.alucard@hotmail.com>
2013-12-03 17:44:39 +0100

    switched layout to use bootstrap.css,
    top div removed (not needed, thanks to dropdowns)

--- public/scripts/modules/ui.js
+++ public/scripts/modules/ui.js
@@ -1,11 +1,6 @@
 ﻿
 define('ui', ['dataservice', 'jquery', 'doT', 'sammy', 'core', 'bootstrap'], function(dataservice, $, doT, sammy){
 
-    var templates = {};
-    templates.link = doT.template($("#template-link").text());
-    templates.message = doT.template($("#template-message").text());
-    templates.comment = doT.template($("#template-comment").text());
-
     function showError(message) {
         $("#content").prepend($(templates.message(message)).addClass("alert-danger"));
     }
@@ -13,5 +8,6 @@ define('ui', ['dataservice', 'jquery', 'doT', 'sammy', 'core', 'bootstrap'], fun
     function showInfo(message) {
         $("#content").prepend($(templates.message(message)).addClass("alert-info"));
     }
-   
+
+
 });
