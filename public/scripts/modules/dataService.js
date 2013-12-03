6a6ce293aa39409b48c76327b1c7cf8c7c44df73
MrHaribo <jonas_biedermann@hotmail.com>
2013-12-03 10:36:37 +0100

    RequireJS

---
+++ public/scripts/modules/dataService.js
@@ -0,0 +1,24 @@
+﻿(function ($) {
+	
+	$( document ).ready(function() {
+		
+		alert("data service rdy");
+	
+	
+		var serverAPI = "http://localhost:4730/entries";
+		  $.getJSON( serverAPI, function( result ) {
+			  
+			  $( "#main" ).html( "Entries:" );
+			  //$( "#main" ).append("<br/>");
+			  
+		      $.each(result, function(i, field){
+		    	  alert(field.url);
+		          //$("#main").append(  field.id + " ");
+		          //$("#main").append(  field.title + " ");
+		          //$("#main").append(  field.url + " ");
+		      });
+		  });
+	  
+	});
+  
+})(jQuery);
