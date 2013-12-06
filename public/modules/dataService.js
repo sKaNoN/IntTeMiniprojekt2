aa8fed9d7ce0fd1b97747e322d7cf018cb40e8de
MrHaribo <jonas_biedermann@hotmail.com>
2013-12-06 13:34:15 +0100

    Merge

--- public/modules/dataService.js
+++ public/modules/dataService.js
@@ -3,7 +3,22 @@
     var dataservice = {
         entry: {
             getAll: function() {
-                return $.getJSON('entries');
+                return $.getJSON('/entries');
+            },
+            get: function(id) {
+                return $.getJSON('entry/' + id);
+            }
+        },
+
+        user: {
+            login: function(username, password) {
+                $.post("login", { name: username, password: password }, function(success){
+                    if (success === true) {
+                        $.event.trigger({ type: "login", name: username });
+                    } else {
+                        $.event.trigger({ type: "login-failed" });
+                    }
+                });
             }
         }
 
