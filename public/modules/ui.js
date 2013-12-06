8bbad93cdf95c68647224467a658895ef7bdd31d
skanon <nosferatu.alucard@hotmail.com>
2013-12-06 08:21:54 +0100

    some form changes

--- public/modules/ui.js
+++ public/modules/ui.js
@@ -1,8 +1,8 @@
-﻿define('ui', ['modules/dataservice', 'knockout-3.0.0', 'sammy', 'modules/core', 'bootstrap'], function(dataservice ,ko, sammy){
+﻿define('ui', ['modules/dataservice', 'ko', 'sammy', 'modules/core', 'bootstrap'], function(dataservice ,ko, sammy){
 
     return function ui() {
        var self = this;
-        
+
        self.user.username = ko.observable('');
        self.user.password = ko.observable('');
        self.user.login = ko.computed(function() {
